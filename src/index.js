6959ba8bd65c7f4e95aeee2d46a96aa193435e08
Kirsawka <83959481+Kirsawka@users.noreply.github.com>
2022-02-28 20:19:27 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+        for (let config of bracketsConfig) {
+            let ind = config.indexOf(currentSymbol);
+            if (ind === 0) {
+                let topElement = stack[stack.length - 1];
+                if ((topElement === currentSymbol) && currentSymbol === config[1]) {
+                    stack.pop();
+                } else {
+                    stack.push(currentSymbol);
+                }
+            } else if (ind === 1) {
+                if (stack.length === 0) {
+                    return false;
+                }
+                let topElement = stack[stack.length - 1];
+                if (config[0] === topElement) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
 }
+
+
+
